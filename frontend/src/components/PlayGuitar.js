a7ddb121f231d6c7607df0d6f73461e5ddca4b9c
Gyugyi Peter <Peter.Gyugyi@kuka.com>
2020-10-25 17:07:11 +0100

    some keyboard events for testing

--- frontend/src/components/PlayGuitar.js
+++ frontend/src/components/PlayGuitar.js
@@ -1,70 +1,110 @@
-import React from "react";
-import { Howl, Howler } from "howler";
+import React, { useContext } from "react";
+import { AppContext } from "./AppContext";
+import { Howler } from "howler";
 import KeyboardEventHandler from "react-keyboard-event-handler";
 import { getDistortedGuitar, getCleanGuitar } from "../domain/NoteFilePairs";
 import GuitarSettings from "./GuitarSettings";
+import GuitarNeck from "./GuitarNeck";
+import { AiOutlineArrowLeft, AiOutlineArrowRight} from "react-icons/ai";
 
-const PlaySound = (src) => {
-  const sound = new Howl({
-    src,
-  });
-  sound.play();
-};
+const notes = ["C", "Db", "D", "Eb", "E", "F", "Gb", "G", "Ab", "A", "Bb", "B"];
 
-function HandleKeyPress() {
-  return(
-  <KeyboardEventHandler
-      handleKeys={["q", "w", "e", "r", "t", "z", "u", "i", "o", "a", "s", "d", "f", "g", "h", "j", "k", "l",
-                    "y", "x", "c", "v", "b", "n", "m", "1", "2", "3", "4", "5", "6", "7", "8", "9"]}
-      onKeyEvent={(key) => {
+function HandleKeyPress(props) {
+  const {
+    addLog,
+    firstNote1,
+    range1,
+    isKeyEventsDisabled,
+    PlaySound,
+  } = useContext(AppContext);
+
+  function chooseNote(index) {
+    if (index < 12) return notes[index] + range1;
+    else return notes[index % 12] + (range1 + 1);
+  }
+
+  function keyDownEvent(indexToAdd, keyPressed, querySelector, event, whichFirstNote, whichFunction, whichGuitarSound) {
+    if (!event.repeat) {
+        addLog({ note: whichFunction(whichFirstNote + indexToAdd), key: keyPressed });
+        PlaySound(whichGuitarSound().get(whichFunction(whichFirstNote + indexToAdd)));
+        document.querySelector(querySelector).classList.add("pressed");
+    }
+  }
+
+  return (
+    <>
+    <KeyboardEventHandler
+      handleFocusableElements
+      isDisabled={isKeyEventsDisabled}
+      handleKeys={["all"]}
+      onKeyEvent={(key, e) => {
         switch (key) {
-          case "q":
-            PlaySound(getCleanGuitar().get("G2"));
-            break;
-          case "w":
-            PlaySound(getCleanGuitar().get("Eb3"));
-            break;
-          case "e":
-            PlaySound(getCleanGuitar().get("G3"));
-            break;
-          case "r":
-            PlaySound(getCleanGuitar().get("D4"));
-            break;
-          case "a":
-            PlaySound(getCleanGuitar().get("Bb2"));
+          case "y":
+            keyDownEvent(0, "y", "#string1Note1", e, firstNote1, chooseNote, getDistortedGuitar);
             break;
           case "s":
-            PlaySound(getCleanGuitar().get("Eb4"));
-            break;
-          case "y":
-            PlaySound(getCleanGuitar().get("Bb3"));
+            keyDownEvent(1, "s", "#string1Note2", e, firstNote1, chooseNote, getCleanGuitar);
             break;
           case "x":
-            PlaySound(getCleanGuitar().get("F4"));
-            break;
-          case "c":
-            PlaySound(getCleanGuitar().get("C3"));
-            break;
-          case "v":
-            PlaySound(getCleanGuitar().get("F3"));
-            break;
-          case "b":
-            PlaySound(getCleanGuitar().get("D2"));
-            break;
-          case "t":
-            PlaySound(getCleanGuitar().get("G4"));
+            keyDownEvent(2, "x", "#string1Note3", e, firstNote1, chooseNote, getDistortedGuitar);
             break;
           default:
         }
       }}
-    />)
-};
+    />
+    
+    <KeyboardEventHandler
+      handleFocusableElements
+      isDisabled={isKeyEventsDisabled}
+      handleEventType = {"keyup"}
+      handleKeys={["alphanumeric"]}
+      onKeyEvent={(key, e) => {
+        switch (key) {
+          case "y":
+            document.querySelector("#string1Note1").classList.remove("pressed");
+            break;
+          case "s":
+            document.querySelector("#string1Note2").classList.remove("pressed");
+            break;
+          case "x":
+            document.querySelector("#string1Note3").classList.remove("pressed");
+            break;
+          default:
+        }
+      }}
+    />
+    </>
+  );
+}
 
 function PlayGuitar(props) {
+  const { firstFretPos, setFirstFretPos } = useContext(AppContext);
   Howler.volume(0.1);
   return (
-    <><HandleKeyPress />
+    <>
+      <HandleKeyPress />
       <GuitarSettings />
+      <div className="guitarNeckPosition">
+        <button
+          className="guitarNeckPositionArrow"
+          disabled= {firstFretPos === 1}
+          onClick={() => {
+            setFirstFretPos(firstFretPos - 1);
+          }}
+        >
+          <AiOutlineArrowLeft />
+        </button>
+        <GuitarNeck />
+        <button
+          className="guitarNeckPositionArrow"
+          disabled= {firstFretPos === 15}
+          onClick={() => {
+            setFirstFretPos(firstFretPos + 1);
+          }}
+        >
+          <AiOutlineArrowRight />
+        </button>
+      </div>
     </>
   );
 }
